c765cca43362a51dd3d74fedb64f094e2d53910e
saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>
2023-12-06 06:04:26 +0000

    A1

--- git.sh
+++ git.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Git add all changes
+git status
 git add .
-
-# Git commit with message "A1"
 git commit -m "A1"
-
-# Git push to the "ent-flow" branch on the "origin" remote
 git push origin ent-flow
