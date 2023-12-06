ee6173b21f6e411ca329c0f301d19706d8b8d299
saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>
2023-12-06 05:41:54 +0000

    A1

---
+++ automate-git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Git add all changes
+git add .
+
+# Git commit with message "A1"
+git commit -m "A1"
+
+# Git push to the "ent-flow" branch on the "origin" remote
+git push origin ent-flow
