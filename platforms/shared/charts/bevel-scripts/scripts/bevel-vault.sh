184a402a07838c67ae69485378cc08aabf991f79
saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>
2023-12-19 12:55:39 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: saurabhkumarkardam <saurabh.kumar.kardam@accenture.com>

--- platforms/shared/charts/bevel-scripts/scripts/bevel-vault.sh
+++ platforms/shared/charts/bevel-scripts/scripts/bevel-vault.sh
@@ -4,8 +4,6 @@
 #  SPDX-License-Identifier: Apache-2.0
 ##############################################################################################
 
-#!/bin/sh
-
 # Function to validate HashiCorp Vault responses
 validateVaultResponseHashicorp() {
     if [ "$2" = "LOOKUPSECRETRESPONSE" ]; then
